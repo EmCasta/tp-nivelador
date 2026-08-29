5a7eafb40cb2d261822f499230bd0d87851abe21
EmCasta <ecastares@fi.uba.ar>
2026-08-29 17:14:13 -0300

    Ejercicio 4: implementacion de short read y short write para cliente

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -1,22 +1,35 @@
 package safe_socket
 
-import "io"
-
-//TODO: Complete with a short-read/short-write tolerant implementation
+import (
+	"io"
+)
 
 func SendAll(socket io.Writer, bytes []byte) error {
-	_, err := socket.Write(bytes)
-	if err != nil {
-		return err
+	bytesWritten := 0
+	for bytesWritten < len(bytes) {
+		n, err := socket.Write(bytes[bytesWritten:])
+		if err != nil {
+			return err
+		}
+		bytesWritten += n
 	}
 	return nil
 }
 
 func RecvAll(socket io.Reader, size int) ([]byte, error) {
-	buff := make([]byte, size)
-	n, err := socket.Read(buff)
-	if err != nil {
-		return nil, err
+	total := make([]byte, 0, size)
+	bytesRead := 0
+	for bytesRead < size {
+		buff := make([]byte, size-bytesRead)
+		n, err := socket.Read(buff)
+		total = append(total, buff[:n]...)
+		bytesRead += n
+		if err != nil {
+			if err == io.EOF {
+				return total, nil
+			}
+			return nil, err
+		}
 	}
-	return buff[:n], nil
+	return total, nil
 }
